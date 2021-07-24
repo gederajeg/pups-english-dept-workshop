0678c4481512ed5cf89108f09e60d2200ccc5737
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2021-07-24 09:34:44 +0800

    add more content to the basic data type in R

--- 0-jenis-data-di-R.R
+++ 0-jenis-data-di-R.R
@@ -1,7 +1,7 @@
 # Sekilas tentang jenis data di R
 
 
-# Vektor (DATA SATU DIMENSI)
+# Vektor (DATA SATU DIMENSI) =========
 ## 1. vektor numerik, yaitu jenis data satu dimensi yang mengandung rangkaian data angka.
 ## Apa kira-kira contoh vektor/variabel numerik yang berisi data angka?
 ### - usia
@@ -44,19 +44,59 @@ str(data_logikal)
 
 
 ## 4. faktor, yaitu jenis data yang *under the hood* "tersimpan sebagai integer unik, yaitu bilangan bulat unik", DAN "memiliki label dalam bentuk karakter". Data yang berjenis faktor terdiri atas komponen/data yang disebut dengan *levels*; R secara otomatis mengurutkan data/komponen dari faktor ini secara alfabetis.
-sex <- c("male", "female", "female", "male") # vektor karakter
+sex <- c("male", "female", "female", "male", "female") # vektor karakter
 typeof(sex)
 str(sex)
 
-sex_factor <- factor(c("male", "female", "female", "male")) # membuat faktor, yaitu integer yang memiliki label karakter
+sex_factor <- factor(c("male", "female", "female", "male", "female")) # membuat faktor, yaitu integer yang memiliki label karakter
 ### R akan memberikan nilai/integer "1" untuk "female" dan "2" untuk "male" karena secara alfabetis, "f" muncul lebih awal daripada "m", meskipun pada saat membuat faktor di atas, "male" ditulis lebih awal dibandingkan "female". Kita bisa buktikan/mengecek ini dengan menjalankan kode berikut:
 levels(sex_factor) # female adalah level pertama, male adalah level kedua
 str(sex_factor)
 typeof(sex_factor) # R menyatakan bahwa faktor sex_factor adalah integer meskipun berlabel karakter!
 
+# kita bisa ubah urutan kategori dalam faktor sex_factor dengan menambahkan argument `levels` dalam fungsi factor()
+# misalnya, ubah urutan dari male (1) ke female (2)
+sex_factor_reverse <- factor(sex_factor, levels = c("male", "female"))
+
+# bandingkan urutan level kedua faktor berikut; isinya tetap sama tapi pengurutan levelnya berubah --- ini akan ada implikasi dalam hal visualisasi nantinya terkait kategori/level mana yang ingin ditampilkan lebih awal (mis. dalam diagram batang).
+sex_factor
+sex_factor_reverse
+
 ### IMPLIKASI Faktor
 ### - Faktor berperan penting dalam analisis statistik dan visualisasi.
+# menyarikan informasi dalam data dengan fungsi summary()
 
+# karena jenis data berupa faktor yang merupakan integer/bil. bulat, summary() akan langsung melakukan count/penghitungan jumlah level/komponen dari faktor jenis kelamin
+summary(sex_factor) 
+summary(sex_factor_reverse)
+# kita bisa langsung gabungkan dengan fungsi visualisasi barplot()
+barplot(summary(sex_factor)) 
+# atau simpan terlebih dahulu luaran summary() lalu masukkan ke fungsi barplot()
+sex_factor_count <- summary(sex_factor)
+barplot(sex_factor_count)
+
+# summary() tidak menghasilkan efek yang sama ketika data berupa karakter
+summary(sex)
+barplot(summary(sex)) # ERROR, kecuali lakukan tabulasi terlebih dahulu dengan table()
+table(sex)
+barplot(table(sex)) # berfungsi
+
+
+# Menyimpan vektor ke dalam file plain teks
+cat(kelas_kata, file = "kelas_kata.txt", sep = "\n")
+cat(usia, file = "usia.txt", sep = "\n")
+
+# Mengimpor vektor ke R
+scan(file = "usia.txt", sep = "\n")
+scan(file = "kelas_kata.txt", what = "char", sep = "\n")
+
+usia <- scan(file = "usia.txt", sep = "\n")
+kelas_kata <- scan(file = "kelas_kata.txt", what = "char", sep = "\n")
+
+# coba impor file plain text berisi vektor karakter dengan nama file "syntactic_function.txt" dan simpan ke dalam objek bernama syn_func.
+
+
+# Data frame (DATA DUA DIMENSI) =========
 # Data frame/tabel, yaitu struktur data dua dimensi (ada BARIS dan KOLOM). Masing-masing kolom bisa berjenis data berbeda, mis. numerik, faktor, karakter, logikal. Data frame bisa dibuat di R dari sekumpulan vektor dengan jumlah komponen yang sama.
 
 ## vektor berikut akan menjadi isian kolom dari data frame/tabel
@@ -68,6 +108,9 @@ pendidikan_terakhir <- c("S1", "S1", "S2", "SMA", "SMK") # karakter
 
 # gunakan fungsi data.frame() untuk membuat data frame
 partisipan <- data.frame(sex, usia, lokasi, apakah_tamat_pt, pendidikan_terakhir) # data kolom dapat diurutkan sesuai keinginan
+
+# lihat tampilan data frame
 partisipan
+
 str(partisipan)
 typeof(partisipan) # data frame merupakan sejenis "list" di R, list yang menyimpan vektor dalam bentuk tabel dua dimensi.
